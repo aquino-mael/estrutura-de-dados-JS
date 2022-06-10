4de75d32f62e9f0ddc77d9297ebdfe20d906023b
Ismael Aquino <ibranco@brasiliano.com.br>
2022-06-09 23:29:45 -0300

    initial commit

---
+++ capitulo-01/variaveis.js
@@ -0,0 +1,19 @@
+var num = 1;
+num = 3;
+var price = 1.5;
+var myName = 'Ismael Aquino';
+var trueValue = true;
+var nullValue = null;
+var undefinedValue;
+
+document.write('The current value on the "num" variable is: ', num);
+document.write('</br>')
+document.write('The price is: ', price);
+document.write('</br>')
+document.write('Hello, ', myName);
+document.write('</br>')
+document.write('Do u haven\'t girlfriend? ', trueValue);
+document.write('</br>')
+document.write('null? ', nullValue);
+document.write('</br>')
+document.write('WHAT THE FUCK?? ', undefinedValue);
